9d7a13b84e1fb6a387ec5553b0e5c7bdeabf2e4a
hans-2002 <pattnaikhans@gmail.com>
2022-10-18 19:11:39 +0530

    UI remake

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,49 +1,63 @@
 import React, { useEffect, useState } from "react";
 
 let globalId = 0;
+
 export default function App() {
 
 
-  const [task, settasks] = useState();
+  const [task, settasks] = useState("");
   const [todos, settodos] = useState([]);
+  const [placeholderText, setPlaceholderText] = useState("Put it in here");
 
   function createTodo(event) {
-
     event.preventDefault();
-
-    settodos(oldtodos => {
-      settasks("")
-      return [...oldtodos, { todo: task, id: globalId++ }]
-    })
-
-
+    if (task !== "") {
+      settodos(oldTodos => {
+        settasks("")
+        return [...oldTodos, { todo: task, id: globalId++ }]
+      })
+    }
+    else {
+      setPlaceholderText("Write something :)");
+      setTimeout(() => {
+        setPlaceholderText("Put it in here");
+      }, 5000);
+    }
   }
 
   function deleteItem(itemID) {
-    settodos(oldtodos => oldtodos.filter(item => item.id !== itemID))
-
-
+    settodos(oldTodos => oldTodos.filter(item => item.id !== itemID));
   }
 
 
-  return <div>
+  return (
+    <>
+      <div className="main">
+        <h1>Stick your TODOs</h1>
+        <form onSubmit={createTodo} className="todo-form">
+          <ul>
+            <div className="Box">
+              <textarea type="text" value={task} placeholder={placeholderText} className='inputBar' onChange={event => {
+                settasks(event.target.value)
+              }} />
+              <button type="submit" className="btn addBtn"><i class="fa-solid fa-plus"></i></button>
+            </div>
 
-    <form onSubmit={createTodo}>
-
-      <h1>MY todo app</h1>
-      <input type="text" value={task} onChange={event => {
-        settasks(event.target.value)
-      }} />
-      <button type="submit">Add task</button>
-    </form>
-
-    <ul>
-      {todos.map((item, index) => {
-        return <div key={item.id}>
-          <li>{item.todo}</li>
-          <button onClick={() => deleteItem(item.id)}>DONE</button>
-        </div>
-      })}
-    </ul>
-  </div>
+            {todos.length !== 0 ? todos.map((item, index) => {
+              return (
+                <>
+                  <div className="Box" key={item.id}>
+                    <li>
+                      <textarea className='inputBar'>{item.todo}</textarea>
+                    </li>
+                    <button className="btn deleteBtn" onClick={() => deleteItem(item.id)}><i class="fa-solid fa-trash"></i></button>
+                  </div>
+                </>
+              );
+            }) : null}
+          </ul>
+        </form>
+      </div>
+    </>
+  );
 }
